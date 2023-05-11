d009948d07f7113643aa56e8b017a9763dbaf9a5
Jillian Dunic <>
2023-05-10 17:38:42 -0700

    compare alternating sampling and 'both' sampling in synoptic data

--- analysis/syn-examples.R
+++ analysis/syn-examples.R
@@ -40,6 +40,7 @@ library(tidyverse)
 library(gfplot)
 library(sdmTMB)
 library(beepr)
+library(patchwork)
 
 source(here::here("analysis", "00_prep-example-data.R"))
 source(here::here("analysis", "utils.R"))
@@ -49,43 +50,58 @@ source(here::here("analysis", "fit-funcs.R"))
 mytheme <- function() ggsidekick::theme_sleek() # sometimes I add more layers to themes
 theme_set(mytheme())
 
+plot_index <- function(df) {
+  df |>
+    ggplot(mapping = aes(x = year, y = est, ymin = lwr, ymax = upr, colour = region)) +
+    geom_pointrange() +
+    geom_ribbon(alpha = 0.20, colour = NA) +
+    scale_colour_manual(values = syn_region_colours$colours, breaks = syn_region_colours$region) +
+    labs(colour = "Sampled region")
+}
+
+
 # Setup inside data and look at survey coverage over time
 # ------------------------------------------------------------------------------
 syn_survey_dat <- dat |>
   filter(str_detect(survey_abbrev, "SYN")) |>
+  filter(!(year %in% c(2003, 2004, 2020))) |> # Use only complete N/S sampling years
   drop_na(trawl_offset) # There are some NA values in the offset
+
+# Consider excluding 2007; or exclude the WCHG from 2007 to make the data more comparable?
 
 # Come back to this because I don't know that the categories are straightforward
 syn_region_colours <- tibble(
-  region = c("SYN QCS", "SYN HS", "SYN WCVI", "SYN WCHG"),
-  colours = c(RColorBrewer::brewer.pal(4L, "Set2"))
+  region = c("QCS + HS", "WCHG + WCVI", "QCS + HS + WCVI", "QCS + HS + WCHG", "No data"),
+  colours = c(RColorBrewer::brewer.pal(4L, "Set2"), "grey70")
 )
 
 ggplot(data = syn_survey_dat) +
   geom_point(aes(x = X, y = Y, colour = survey_abbrev)) +
   scale_colour_manual(
-    values = syn_region_colours$colours,
-    breaks = syn_region_colours$region, na.translate = FALSE
+    values = RColorBrewer::brewer.pal(4L, "Set2"),
+    breaks = c("SYN QCS", "SYN HS", "SYN WCVI", "SYN WCHG"), na.translate = FALSE
   ) +
   facet_wrap(~fyear, drop = FALSE)
 
+dir.create('figs')
+ggsave(here::here("figs", "synoptic-survey-coverage.png"), width = 7.5, height = 6.5)
+
 # Come back to this because I don't know that the categories are straightforward
 syn_survey_yrs <-
-  dat |>
+  syn_survey_dat |>
   filter(str_detect(survey_abbrev, "SYN")) |>
-  distinct(year, survey_abbrev) #|>
-# arrange(year) |>
-# rename(region = "survey_abbrev") |>
-# complete(year = 2003:2022, fill = list(region = "None", surveyed = FALSE)) |>
-# mutate(surveyed = 1) |>
-# pivot_wider(id_cols = year, names_from = region, values_from = surveyed) |>
-# mutate(region = case_when(
-#   `HBLL INS N` == 1 & is.na(`HBLL INS S`) & is.na(None) ~ "HBLL INS N",
-#   is.na(`HBLL INS N`) & `HBLL INS S` == 1 & is.na(None) ~ "HBLL INS S",
-#   `HBLL INS N` == 1 & `HBLL INS S` == 1 & is.na(None) ~ "Both",
-#   is.na(`HBLL INS N`) & is.na(`HBLL INS S`) & None == 1 ~ "No data"
-# )) |>
-# select(year, region)
+  distinct(year, survey_abbrev) |>
+  arrange(year) |>
+  rename(region = "survey_abbrev") |>
+  mutate(surveyed = 1) |>
+  pivot_wider(id_cols = year, names_from = region, values_from = surveyed) |>
+  mutate(region = ifelse(year %% 2 == 0, "QCS + HS", "WCHG + WCVI")) |>
+  mutate(
+    region = replace(region, year == 2007, "QCS + HS + WCHG"),
+    region = replace(region, year == 2021, "QCS + HS + WCVI")
+  ) |>
+  select(year, region) |>
+  bind_rows(tibble(year = 2020, region = "No data"))
 
 fitted_yrs_extra <- min(syn_survey_dat$year):max(syn_survey_dat$year)
 
@@ -98,56 +114,109 @@ syn_nd <-
 
 # Arrowtooth & Bocaccio examples
 # ------------------------------------------------------------------------------
+get_syn_index <- function(sp_dat) {
+  future::plan(future::multisession, workers = 5) # or whatever number
+
+  sp_dat <-
+    sp_dat |>
+    group_by(species_common_name) |>
+    group_split()
+
+  map_func <- furrr::future_map
+  # map_func <- purrr::map
+
+  fits1 <- sp_dat |>
+    map_func(
+      fit_models,
+      catch = "catch_weight", silent = FALSE,
+      family = tweedie(), offset = "trawl_offset", data_subset = "data_subset"
+    ) |>
+    list_flatten(name_spec = "{inner}")
+  future::plan(future::sequential)
+
+  fits_cleaned1 <- fits1 |>
+    map(check_sanity) # omit plots made from models that did not pass sanity check
+
+  message("Getting predictions")
+  preds1 <- get_pred_list(fits_cleaned1, newdata = syn_nd)
+  message("Getting index")
+  indices1 <- get_index_list(pred_list = preds1)
+
+  indices1
+}
+
+# ------------------------------------------------------------------------------
+# Synoptic trawl example species
 spp <- c("arrowtooth flounder", "bocaccio")
+
+# Strict alternating
+# -----------------------
 sp_dat <- syn_survey_dat |>
   filter(species_common_name %in% spp) |>
-  mutate(data_subset = paste(species_common_name, "Stitched N/S", sep = "-")) |>
-  group_by(species_common_name) |>
-  group_split()
-
-future::plan(future::multisession, workers = 5) # or whatever number
-map_func <- furrr::future_map
-# map_func <- purrr::map
-
-fits1 <- sp_dat %>%
-  # furrr::future_map(fit_models, catch = "density_ppkm2", silent = FALSE) %>%
-  # map(fit_models, catch = "density_ppkm2", silent = FALSE, family = sdmTMB::delta_gamma())
-  # map(fit_models, catch = "density_ppkm2", silent = FALSE, family = sdmTMB::tweedie())
-  map_func(
-    fit_models,
-    catch = "catch_weight", silent = FALSE,
-    family = tweedie(), offset = "trawl_offset", data_subset = "data_subset"
+  filter(
+    !(year == 2007 & region == "SYN WCHG"),
+    !(year == 2020),
+    !(year == 2021 & region == "SYN WCVI")
   ) |>
-  list_flatten(name_spec = "{inner}")
-beep()
-future::plan(future::sequential)
+  mutate(data_subset = paste(species_common_name, "Strict alternating N/S", sep = "-"))
 
-fits_cleaned1 <- fits1 |>
-  map(check_sanity) # omit plots made from models that did not pass sanity check
+# alternating_ind_list <- get_syn_index(sp_dat)
+# saveRDS(alternating_ind_list, here::here('data-outputs', "syn_strict-alternating_ind-list.RDS"))
+alternating_ind_list <- readRDS(here::here("data-outputs", "syn_strict-alternating_ind-list.RDS"))
 
-saveRDS(fits_cleaned1, "syn-fits.RDS")
-fits_cleaned1 <- readRDS("syn-fits.RDS")
+alternating_ind_df <- mk_index_df(alternating_ind_list) |>
+  separate(group, into = c("species", "group"), sep = "-") |>
+  left_join(syn_survey_yrs, by = "year") |>
+  mutate(region = ifelse(year %% 2 == 0, "QCS + HS", "WCHG + WCVI")) |>
+  mutate(region = replace(region, year == 2020, "No data")) |>
+  left_join(syn_region_colours, by = "region")
 
-preds1 <- get_pred_list(fits_cleaned1, newdata = syn_nd)
-indices1 <- get_index_list(pred_list = preds1)
-beep()
+# Including broader spatial coverage: 2007 WCHG + QCS/HS & 2021 WCVI + QCS/HS &
+# ------------------------------------------------------------------------------
+sp_dat <- syn_survey_dat |>
+  filter(species_common_name %in% spp) |>
+  mutate(data_subset = paste(species_common_name, "Additional region sampling", sep = "-"))
 
-ids <- names(indices1) %>%
-  strsplit(":") %>%
-  purrr::map(~ .x[[1]]) %>%
-  unlist() %>%
-  unique()
-lu <- tibble(id = seq_along(ids), desc = ids, order = as.integer(id))
+# both_regions_ind_list <- get_syn_index(sp_dat)
+# beep()
+# saveRDS(both_regions_ind_list, here::here('data-outputs', "syn_extended-sp-coverage_ind-list.RDS"))
+both_regions_ind_list <- readRDS(here::here("data-outputs", "syn_extended-sp-coverage_ind-list.RDS"))
 
-index_df1 <- mk_index_df(indices1) |>
-  left_join(lu, by = join_by(desc)) |>
-  rename(species = "group")
+both_regions_df <- mk_index_df(both_regions_ind_list) |>
+  separate(group, into = c("species", "group"), sep = "-") |>
+  left_join(syn_survey_yrs, by = "year") |>
+  left_join(syn_region_colours, by = "region")
+
+combined_ind_df <- bind_rows(alternating_ind_df, both_regions_df)
 
 p1 <-
-  ggplot(data = index_df1, aes(x = year, y = est, ymin = lwr, ymax = upr)) +
-  geom_pointrange() +
-  geom_ribbon(alpha = 0.20, colour = NA) +
-  labs(colour = "Sampled region") +
-  facet_wrap(species ~ fct_reorder(desc, order), nrow = 4L, scales = "free_y") +
-  ggtitle("Stitched N/S - All Years")
-p1
+  combined_ind_df |>
+  filter(species == "arrowtooth flounder", group == "Strict alternating N/S") |>
+  plot_index() +
+  facet_wrap(~ fct_reorder(desc, id), nrow = 2L, scales = "free_y", as.table = TRUE) +
+  ggtitle("Arrowtooth Flounder - Alternating N/S sampling")
+
+p2 <-
+  combined_ind_df |>
+  filter(species == "arrowtooth flounder", group == "Additional region sampling") |>
+  plot_index() +
+  facet_wrap(~ fct_reorder(desc, id), nrow = 2L, scales = "free_y", as.table = TRUE) +
+  ggtitle("Arrowtooth Flounder - Additional region coverage")
+
+p3 <-
+  combined_ind_df |>
+  filter(species == "bocaccio", group == "Strict alternating N/S") |>
+  plot_index() +
+  facet_wrap(~ fct_reorder(desc, id), nrow = 2L, scales = "free_y", as.table = TRUE) +
+  ggtitle("Bocaccio - Alternating N/S sampling")
+
+p4 <-
+  combined_ind_df |>
+  filter(species == "bocaccio", group == "Additional region sampling") |>
+  plot_index() +
+  facet_wrap(~ fct_reorder(desc, id), nrow = 2L, scales = "free_y", as.table = TRUE) +
+  ggtitle("Bocaccio - Additional region coverage")
+
+p1 / p2
+
+p3 / p4
