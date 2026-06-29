d8c07fa906cf6fed9193a707db456826e9fce09c
Sean Anderson <sean@seananderson.ca>
2026-06-29 15:08:56 -0700

    Plots

--- analysis/03-bc-seesaw-testing.R
+++ analysis/03-bc-seesaw-testing.R
@@ -129,13 +129,13 @@ do_fit <- function(.sp, .survey) {
     return(dplyr::tibble())
   }
 
-  # fits[["IID RF, factor(year), all data"]] <- safe_fit(update(
-  #   fits[[base_model]],
-  #   data = dat_all,
-  #   mesh = mesh_all,
-  #   offset = log(dat_all$effort)
-  # ))
-  #
+  fits[["IID RF, factor(year), all data"]] <- safe_fit(update(
+    fits[[base_model]],
+    data = dat_all,
+    mesh = mesh_all,
+    offset = log(dat_all$effort)
+  ))
+
   fits[["IID RF, factor(year), factor(survey), all data"]] <- safe_fit(update(
     fits[[base_model]],
     formula. = . ~ factor(year) + factor(survey_name),
@@ -144,67 +144,67 @@ do_fit <- function(.sp, .survey) {
     offset = log(dat_all$effort)
   ))
 
-  # fits[["RW RF"]] <- safe_fit(update(
-  #   fits[[base_model]],
-  #   formula. = . ~ 1,
-  #   spatiotemporal = "rw",
-  #   extra_time = all_yrs
-  # ))
-  #
-  # fits[["AR1 RF"]] <- safe_fit(update(
-  #   fits[[base_model]],
-  #   formula. = . ~ 1,
-  #   spatiotemporal = "ar1",
-  #   extra_time = all_yrs
-  # ))
-  #
-  # fits[["RW RF, RW year"]] <- safe_fit(update(
-  #   fits[[base_model]],
-  #   formula. = . ~ 1,
-  #   spatiotemporal = "rw",
-  #   time_varying = ~1,
-  #   time_varying_type = "rw0",
-  #   priors = sdmTMB::sdmTMBpriors(sigma_V = sdmTMB::gamma_cv(0.3, 0.5)),
-  #   extra_time = all_yrs
-  # ))
-  #
-  # fits[["AR1 RF, RW year"]] <- safe_fit(update(
-  #   fits[[base_model]],
-  #   formula. = . ~ 1,
-  #   spatiotemporal = "ar1",
-  #   time_varying = ~1,
-  #   time_varying_type = "rw0",
-  #   priors = sdmTMB::sdmTMBpriors(sigma_V = sdmTMB::gamma_cv(0.3, 0.5)),
-  #   extra_time = all_yrs
-  # ))
-  #
-  # fits[["IID RF, RW year"]] <- safe_fit(update(
-  #   fits[[base_model]],
-  #   formula. = . ~ 1,
-  #   spatiotemporal = "iid",
-  #   time_varying = ~1,
-  #   time_varying_type = "rw0",
-  #   priors = sdmTMB::sdmTMBpriors(sigma_V = sdmTMB::gamma_cv(0.3, 0.5)),
-  #   extra_time = all_yrs
-  # ))
-  #
-  # fits[["Spatial only, RW year"]] <- safe_fit(update(
-  #   fits[[base_model]],
-  #   formula. = . ~ 1,
-  #   spatiotemporal = "off",
-  #   time_varying = ~1,
-  #   time_varying_type = "rw0",
-  #   priors = sdmTMB::sdmTMBpriors(sigma_V = sdmTMB::gamma_cv(0.3, 0.5)),
-  #   extra_time = all_yrs
-  # ))
-  #
-  # fits[["IID RF, factor(year), depth"]] <- safe_fit(update(
-  #   fits[[base_model]],
-  #   formula. = . ~ 1,
-  #   spatiotemporal = "off",
-  #   time_varying = ~1,
-  #   time_varying_type = "rw0"
-  # ))
+  fits[["RW RF"]] <- safe_fit(update(
+    fits[[base_model]],
+    formula. = . ~ 1,
+    spatiotemporal = "rw",
+    extra_time = all_yrs
+  ))
+
+  fits[["AR1 RF"]] <- safe_fit(update(
+    fits[[base_model]],
+    formula. = . ~ 1,
+    spatiotemporal = "ar1",
+    extra_time = all_yrs
+  ))
+
+  fits[["RW RF, RW year"]] <- safe_fit(update(
+    fits[[base_model]],
+    formula. = . ~ 1,
+    spatiotemporal = "rw",
+    time_varying = ~1,
+    time_varying_type = "rw0",
+    priors = sdmTMB::sdmTMBpriors(sigma_V = sdmTMB::gamma_cv(0.3, 0.5)),
+    extra_time = all_yrs
+  ))
+
+  fits[["AR1 RF, RW year"]] <- safe_fit(update(
+    fits[[base_model]],
+    formula. = . ~ 1,
+    spatiotemporal = "ar1",
+    time_varying = ~1,
+    time_varying_type = "rw0",
+    priors = sdmTMB::sdmTMBpriors(sigma_V = sdmTMB::gamma_cv(0.3, 0.5)),
+    extra_time = all_yrs
+  ))
+
+  fits[["IID RF, RW year"]] <- safe_fit(update(
+    fits[[base_model]],
+    formula. = . ~ 1,
+    spatiotemporal = "iid",
+    time_varying = ~1,
+    time_varying_type = "rw0",
+    priors = sdmTMB::sdmTMBpriors(sigma_V = sdmTMB::gamma_cv(0.3, 0.5)),
+    extra_time = all_yrs
+  ))
+
+  fits[["Spatial only, RW year"]] <- safe_fit(update(
+    fits[[base_model]],
+    formula. = . ~ 1,
+    spatiotemporal = "off",
+    time_varying = ~1,
+    time_varying_type = "rw0",
+    priors = sdmTMB::sdmTMBpriors(sigma_V = sdmTMB::gamma_cv(0.3, 0.5)),
+    extra_time = all_yrs
+  ))
+
+  fits[["IID RF, factor(year), depth"]] <- safe_fit(update(
+    fits[[base_model]],
+    formula. = . ~ 1,
+    spatiotemporal = "off",
+    time_varying = ~1,
+    time_varying_type = "rw0"
+  ))
 
   indexes <- purrr::map(fits, safe_index) |>
     dplyr::bind_rows(.id = "model")
@@ -242,19 +242,19 @@ spp_to_fit_hbll <- c(
   "rougheye/blackspotted rockfish",
   "china rockfish",
   "copper rockfish",
-  "redbanded rockfish",
+  # "redbanded rockfish",
   "north pacific spiny dogfish",
   "tiger rockfish",
   "lingcod",
   "canary rockfish",
   "quillback rockfish",
-  "shortspine thornyhead",
+  # "shortspine thornyhead",
   "yelloweye rockfish",
   "silvergray rockfish",
   "spotted rockfish",
   "big skate",
   "rosethorn rockfish",
-  "souther rock sole",
+  "southern rock sole",
   "longnose skate",
   "pacific cod",
   "arrowtooth flounder"
@@ -273,13 +273,22 @@ saveRDS(out, file = "data-generated/bc-indexes3.rds")
 out <- readRDS("data-generated/bc-indexes.rds")
 out2 <- readRDS("data-generated/bc-indexes2.rds") |>
   filter(model != "IID RF, factor(year)")
-out <- bind_rows(out, out2)
+out3 <- readRDS("data-generated/bc-indexes3.rds") |>
+  filter(model != "IID RF, factor(year)")
+out <- bind_rows(out, out2, out3) |>
+  filter(!species %in% c("redbanded rockfish", "shortspine thornyhead"))
 
+dat <- surveyjoin::get_data("pacific cod", regions = "pbs") |>
+  mutate(year = lubridate::year(lubridate::ymd(date))) |> 
+  filter(!(year %in% c(2003, 2004, 2020))) |>
+  tidyr::drop_na(effort, catch_weight, depth_m) |>
+  filter(!(year == 2007 & survey_name == "SYN WCHG")) |>
+  filter(!(year == 2021 & survey_name == "SYN WCVI"))
 lu <- data.frame(year = sort(unique(dat$year)))
 lu$even <- lu$year %% 2 == 0
 lu$survey_group <- ifelse(lu$even, "WCHG + WCVI", "QCS + HS")
 
-moving_window_acf <- function(x, window = 8L) {
+moving_window_acf <- function(x, window = 10L) {
   n <- length(x)
   if (n < window) {
     return(NA_real_)
@@ -288,6 +297,19 @@ moving_window_acf <- function(x, window = 8L) {
     acf(x[i:(i + window - 1L)], plot = FALSE)$acf[2L]
   }, numeric(1L))
   min(acf_vals, na.rm = TRUE)
+}
+
+moving_window_amp <- function(x, window = 10L) {
+  n <- length(x)
+  if (n < window) {
+    return(NA_real_)
+  }
+  amp_log <- function(y) mean(abs(diff(y, differences = 2))) / 4
+
+  amp_val <- vapply(seq_len(n - window + 1L), function(i) {
+    amp_log(x[i:(i + window - 1L)])
+  }, numeric(1L))
+  max(amp_val, na.rm = TRUE)
 }
 
 x <- out |>
@@ -336,15 +358,75 @@ ggsave("figs/bc-trawl-acf.pdf", width = 4.5, height = 3.5)
 out |>
   left_join(lu) |>
   group_by(species, model) |>
-  summarise(seesaw_index = moving_window_acf(log_est, window = 10)) |>
+  summarise(
+    min_acf = moving_window_acf(log_est, window = 10),
+    max_amp = moving_window_amp(log_est, window = 10)
+  ) |>
   group_by(model) |>
-  mutate(mean_acf = mean(seesaw_index)) |>
-  ggplot(aes(forcats::fct_reorder(model, mean_acf), seesaw_index)) +
-  geom_point(position = position_jitter(width = 0)) +
+  mutate(mean_acf = mean(min_acf)) |>
+  ggplot(aes(forcats::fct_reorder(model, mean_acf), min_acf)) +
+  geom_point(position = position_jitter(width = 0), mapping = aes(colour = max_amp)) +
+  scale_colour_viridis_c() +
   coord_flip() +
   ylab("Most negative lag-1 autocorrelation\nacross 10-year windows") +
-  scale_colour_gradient2(mid = "grey80") +
   ggsidekick::theme_sleek() +
   theme(axis.title.y = element_blank(), panel.grid.major = element_line(colour = "grey90", linewidth = 0.3), panel.grid.minor = element_line(colour = "grey90", linewidth = 0.3))
 
-ggsave("figs/bc-trawl-acf-moving-window.pdf", width = 4.5, height = 3.5)
+make_fig <- function(what, .ylab = "", include_all_data = FALSE) {
+  if (!include_all_data) {
+    out1 <- out |>
+      filter(!grepl("all data", model)) |>
+      mutate(all_data = FALSE)
+  } else {
+    out1 <- out |> mutate(all_data = grepl("all data", model))
+  }
+
+  log_se_to_cv <- function(sigma_log) sqrt(exp(sigma_log^2) - 1)
+
+  out1 <- out1 |>
+    left_join(lu, by = join_by(year)) |>
+    summarise(
+      min_acf = moving_window_acf(log_est, window = 10),
+      max_amp = moving_window_amp(log_est, window = 10),
+      mean_se = mean(log_se_to_cv(se)),
+      .by = c(species, model, all_data)
+    ) |>
+    group_by(model) |>
+    mutate(mean_acf = mean(min_acf)) |>
+    ungroup() |>
+    mutate(model = forcats::fct_reorder(model, mean_acf))
+
+  g <- out1 |>
+    ggplot(aes(model, {{ what }})) +
+    coord_flip() +
+    ylab(.ylab) +
+    ggsidekick::theme_sleek() +
+    theme(axis.title.y = element_blank(), panel.grid.major = element_line(colour = "grey90", linewidth = 0.3), panel.grid.minor = element_line(colour = "grey90", linewidth = 0.3))
+
+  if (include_all_data) {
+    blue <- RColorBrewer::brewer.pal(8, "Blues")[3]
+    orange <- RColorBrewer::brewer.pal(8, "Oranges")[3]
+    g <- g + geom_violin(data = out1, scale = "width", mapping = aes(colour = all_data, fill = all_data)) + 
+      scale_colour_manual(values = c(blue, orange)) + 
+      scale_fill_manual(values = c(blue, orange)) + 
+      guides(colour = "none", fill = "none")
+  } else {
+    blue <- RColorBrewer::brewer.pal(8, "Blues")[3]
+    g <- g + geom_violin(scale = "width", colour = blue, fill = blue)
+  }
+
+  g + geom_point(position = position_jitter(width = 0), colour = "grey25")
+}
+
+g1 <- make_fig(min_acf, "Most negative\nlag-1 autocorrelation\nacross 10-year windows")
+g2 <- make_fig(max_amp, "Maximum biennial\noscillation amplitude\nacross 10-year windows") +
+  coord_flip(ylim = c(0, 1.05), expand = FALSE)
+g3 <- make_fig(mean_se, "Mean CV")
+patchwork::wrap_plots(g1, g2, axes = "collect")
+ggsave("figs/bc-trawl-acf-moving-window.pdf", width = 6, height = 3.5)
+
+g1 <- make_fig(min_acf, "Most negative\nlag-1 autocorrelation\nacross 10-year windows", include_all_data = TRUE)
+g2 <- make_fig(max_amp, "Maximum biennial\noscillation amplitude\nacross 10-year windows", include_all_data = TRUE) +
+  coord_flip(ylim = c(0, 1.05), expand = FALSE)
+patchwork::wrap_plots(g1, g2, axes = "collect")
+ggsave("figs/bc-trawl-acf-moving-window2.pdf", width = 7, height = 3.5)
